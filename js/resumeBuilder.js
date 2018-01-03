12e138c177f69315c90e0504781fe4ba7382a6a4
Abdulmohsen Alkhamis <Abdulmohsen-alkhamis@hotmail.com>
2018-01-03 21:52:06 +0300

    Map & Display functions
    Adding google map and creating disply function for the objects: education, work.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,18 +2,9 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-// Abdulmohsen
-//$("#main").append("Abdulmohsen Alkhamis");
-//
-//var awesomeThoughts = "I am Abdulmohsen and I am AWESOME!";
-//console.log(awesomeThoughts);
-//var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-//console.log(funThoughts);
-//$("#main").append(funThoughts);
-
 var formattedName = HTMLheaderName.replace("%data%", "Abdulmohsen Alkhamis");
 
-var role = "Web Developer";
+var role = "OSS Administrator";
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 
 $("#header").prepend(formattedRole);
@@ -23,13 +14,13 @@ $("#header").prepend(formattedName);
 
 var bio = {
     "name" : "Abdulmohsen Alkhamis",
-    "role" : "Web Developer",
-    "contact" : {
+    "role" : "OSS Administrator",
+    "contacts" : {
         "mobile": "+966-550708844",
         "email": "abdulmohsen-alkhamis@hotmail.com",
         "github": "Abdulmohsen-91",
         "twitter": "@abdulmohsen__91",
-        "location": "Riyadh - Saudi Arabia"
+        "location": "Riyadh"
     },
     "welcomeMessage": "hi there",
     "skills": ["aaaaaa","bbbbb","cccccc"],
@@ -62,20 +53,20 @@ var work = {
 
 var education = {
     
-    "shcools": [
+    "schools": [
         {
             "name": "Flinders University",
-            "location": "Adelaide - South Australia",
+            "location": "Adelaide",
             "degree": "Associate degree",
-            "majors": [""],
+            "majors": ["aaa","ddd"],
             "dates": "2007",
             "url": "example"
         },
         {
             "name": "Flinders University",
-            "location": "Adelaide - South Australia",
+            "location": "Adelaide",
             "degree": "Associate degree",
-            "majors": [""],
+            "majors": ["eee","eee"],
             "dates": "2013-2015",
             "url": "example"
         },
@@ -83,9 +74,15 @@ var education = {
 
     "onlineCourses": [
         {
-            "title": "",
-            "school": "",
+            "title": "FEND",
+            "school": "Udacity",
             "date": "2018",
+            "url": "example"
+        },
+        {
+            "title": "JS",
+            "school": "Online",
+            "date": "2017",
             "url": "example"
         }
     ] 
@@ -100,19 +97,41 @@ var projects = {
             "title": "Mindset",
             "dates": "Nov",
             "description": "example",
-            "images": ["http://placekitten.com/200/300","2nd image"]
+            "images": ["http://via.placeholder.com/350x150","http://via.placeholder.com/350x150"]
         },
         {
             "title": "Animal",
             "dates": "Dec",
             "description": "example",
-            "images": ["1st image","2nd image"]
+            "images": ["http://via.placeholder.com/350x150","http://via.placeholder.com/350x150"]
+        },
+        {
+            "title": "Portfolio",
+            "dates": "Dec",
+            "description": "example",
+            "images": ["http://via.placeholder.com/350x150","http://via.placeholder.com/350x150"]
         }
+        
     ] 
 };
 
+work.display = function () {
+    for (var i = 0; i < work.jobs.length; i++) {
+        $("#workExperience").append(HTMLworkStart);
+    var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[i].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[i].title);
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;    
+    $(".work-entry:last").append(formattedEmployerTitle);
+    var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
+    $(".work-entry:last").append(formattedLocation);
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
+    $(".work-entry:last").append(formattedDates);
+    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+    $(".work-entry:last").append(formattedDescription);
+    }
+}
+work.display();
 
-// There is an issue with the below function - the image part
 
 projects.display = function() {
     for (var i = 0; i < projects.projects.length; i++) {
@@ -126,21 +145,45 @@ projects.display = function() {
         $(".project-entry:last").append(formattedDescription);
         
         if (projects.projects[i].images.length > 0) {
-            for (image in projects.projects[i].images) {            
-                var formattedImage = HTMLprojectImage.replace("%data%",projects.projects[i].images[image]);
+            var formattedImage = "";
+            for (var x = 0; x < projects.projects[i].images.length; x++) {
+                formattedImage = HTMLprojectImage.replace("%data%",projects.projects[i].images[x]);
+                
                 $(".project-entry:last").append(formattedImage);
             }
-// the below for loop is not working well
-//            for (var x = 0; x < projects.projects.[i].images.length; x++) {
-//                var formattedImage = HTMLprojectImage.replace("%data%",projects.projects[i].images[x]);
-//                
-//                $(".project-entry:last").append(formattedImage);
-//            }
         }
     }
-}
-$("#projects").append(projects.display());
+};
+projects.display();
 
+// the Below 2 for loops need to be fixed, for example, nested for loop
+education.display = function() {
+    for (var i = 0; i < education.schools.length ; i++) {
+        $("#education").append(HTMLschoolStart);
+        
+        var formattedName = HTMLschoolName.replace("%data%",education.schools[i].name);
+        var formattedDegree = HTMLschoolDegree.replace("%data%",education.schools[i].degree);
+        $(".education-entry:last").append(formattedName + formattedDegree);
+        var formattedDates = HTMLschoolDates.replace("%data%",education.schools[i].dates);
+        $(".education-entry:last").append(formattedDates);
+        var formattedLocation = HTMLschoolLocation.replace("%data%",education.schools[i].location);
+        $(".education-entry:last").append(formattedLocation);   
+        var formattedMajor = HTMLschoolMajor.replace("%data%",education.schools[i].majors);
+        $(".education-entry:last").append(formattedMajor);
+    }
+    for (var x = 0; x < education.onlineCourses.length ; x++) {
+        $("#education").append(HTMLonlineClasses);
+        
+        var formattedTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[x].title);
+        var formattedSchool = HTMLonlineSchool.replace("%data%",education.onlineCourses[x].school);
+        $(".education-entry:last").append(formattedTitle + formattedSchool);
+        var formattedDates = HTMLonlineDates.replace("%data%",education.onlineCourses[x].date);
+        $(".education-entry:last").append(formattedDates);
+        var formattedURL = HTMLonlineURL.replace("%data%",education.onlineCourses[x].url);
+        $(".education-entry:last").append(formattedURL);
+    }
+};
+education.display();
 
 
 
@@ -153,21 +196,6 @@ if (bio.skills.length > 0) {
         $("#skills").append(formattedSkill);
     }
 }
-
-function displayWork() {
-    for (var i = 0; i < work.jobs.length; i++) {
-        $("#workExperience").append(HTMLworkStart);
-    var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[i].employer);
-    var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[i].title);
-    var formattedEmployerTitle = formattedEmployer + formattedTitle;    
-    $(".work-entry:last").append(formattedEmployerTitle);
-    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
-    $(".work-entry:last").append(formattedDates);
-    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
-    $(".work-entry:last").append(formattedDescription);
-    }
-}
-displayWork();
 
 $(document).click(function(loc) {
     var x = loc.pageX;
@@ -185,3 +213,5 @@ function inName(name) {
     return name[0] +" "+name[1];
 }
 $("#main").append(internationalizeButton);
+
+$("#mapDiv").append(googleMap);
